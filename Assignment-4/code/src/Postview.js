4988d04d0acf50f73e10a5fc8af7d887e69a236d
Yash <ybajpai48@gmail.com>
2022-02-16 19:11:33 +0530

    submit-Assignmet4_react

--- Assignment-4/code/src/Postview.js
+++ Assignment-4/code/src/Postview.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Postview.css';
-
+import axios from "axios";
+import logo from "./images/logo.png"
+import camera from "./images/camera.png"
 
 const Postview=()=> {
+  const[post,setPost]=useState([]);
+  React.useEffect(() => {
+    axios.get("http://localhost:3004/user").then((res) => {
+      setPost(res.data);
+    });
+},[]);
   return (
-    <div className="site-container">
-    </div>
+       <>
+      <nav className="navbar">
+      <div className="logo">
+          <img src={logo} width="58" alt="logo"/>
+        </div>
+        <div className="name">
+          <h1>Instaclone</h1>
+        </div>
+        <div className="camera">
+          <img src={camera} alt="camera"/>
+        </div>
+      </nav>
+
+      {post.map((post, idx) => {
+        return (
+          <>
+            <div className="main-info" key={idx} style={{ margin: "20px 0" }}>
+              <aside></aside>
+              <section className="main-container">
+                <header>
+                  <div className="name-loc">
+                    <h3>{post.name}</h3>
+                    <span>{post.location}</span>
+                  </div>
+                </header>
+             <div className="image-container">
+                  <img src={post.PostImage} alt="post-pic" />
+                </div>
+
+                <footer>
+                  <div className="likes-share-date">
+                    <div className="like-share">
+                      
+                    </div>
+                    <div className="date">
+                      {new Date(post.date).toLocaleDateString()}
+                    </div>
+                  </div>
+                  <button>{post.likes} likes</button>
+                  <h2>{post.description}</h2>
+                </footer>
+              </section>
+              <aside></aside>
+            </div>
+          </>
+        );
+      })}
+    </>
+        
   );
-}
+}  
 
 export default Postview;
+     
